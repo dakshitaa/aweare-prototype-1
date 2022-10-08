704c053a9eb5430c4836035396dc9fbb7048b71b
Ramana <ramanalfc@yahoo.com.sg>
2022-10-08 14:06:11 +0800

    [Smart Contract] Update location when tagged

--- init.py
+++ init.py
@@ -1,6 +1,7 @@
 import smartpy as sp
 
 import geocoder
+import datetime
 
 home_add = geocoder.ip('me')
 #initilalise - user id, tag id, home geolocation
@@ -8,21 +9,33 @@ home_add = geocoder.ip('me')
 # + boolean (geolocation of verification != home geolocation)
 # query NFT level
 
-def getCurrLoc():
-    curr_loc = geocoder.ip('me')
-class PhraseKeeper(sp.Contract):
-    def __init__(self, initialPhrase):
-        self.init(phrase = initialPhrase)
-    @sp.entry_point
-    def setPhrase(self, params):
-        self.data.phrase = params
+class GeoTag(sp.Contract):
+    def __init__(self):
+        self.init(home_loc = [0,0], curr_loc = [0,0], userID = 0, tagID = 0, curr_time = str(datetime.datetime.now().time()))
 
-@sp.add_test(name = "Initial Test")
+    @sp.entry_point
+    def setHomeAdd(self, loc):
+        self.data.home_loc = loc
+        self.data.curr_loc = loc
+        self.data.curr_time = str(datetime.datetime.now().time())
+        self.data.tagID += 1
+        self.data.userID += 1
+    
+    @sp.entry_point
+    def setNewTag(self, loc):
+        # sp.verify(loc != home_loc)
+        self.data.curr_loc = loc
+        self.data.curr_time = str(datetime.datetime.now().time())
+        self.data.tagID += 1
+
+@sp.add_test(name = "Test Contract")
 def test():
     scenario = sp.test_scenario()
-    scenario.h1("Initial String")
+    scenario.h1("Initial GeoTag")
     
-    c1 = PhraseKeeper(str(home_add.latlng))
+    c1 = GeoTag()
     scenario += c1
-    scenario.h2("Update String")
-    scenario += c1.setPhrase("Test Success")
+    scenario.h2("Update Home Address (Initial Registration)")
+    scenario += c1.setHomeAdd([107, 120])
+    scenario.h2("First Tag")
+    scenario += c1.setNewTag([103, 99])
